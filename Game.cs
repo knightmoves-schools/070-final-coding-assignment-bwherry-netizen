0ce4426e3c64cd103943ba080d8401b7d3719213
Brendon W. <bwherry@midland.k12.ia.us>
2026-04-09 12:43:40 -0500

    Update Game.cs

--- Game.cs
+++ Game.cs
@@ -1,19 +1,68 @@
+using System;
+using System.Linq;  // <-- add this for Distinct()
+
+namespace knightmoves;
+
 class Game{
     // Make your changes in this file
 
+	private string phrase;
+	
     public Game(string phrase)
     {
+		this.phrase = phrase;
+	}
+
+    public string DisplayBlanks()
+{
+    string result = "";
+
+    foreach (char c in phrase)
+    {
+        if (c == ' ')
+    {
+        result += "  ";
+    }
+    else
+    {
+        result += "_ ";
+    }
     }
 
-    public string DisplayBlanks(){
-        return "_ _ _ _  _ _ _ _  _ _ _ _  _ _ _  _ _ _ _";
-    }
+    return result.TrimEnd();
+}
+    
+public string Play(char[] guessedLetters)
+{
+    string result = "";
+    
+    string guess = new string(guessedLetters).ToUpper();
 
-    public string Play(char[] guessedLetters){
-        return "MAKE THIS WORK FOR REAL";
-    }
+   foreach (char c in phrase)
+{
+    if (c == ' ')
+        result += "  ";
+    else if (guess.Contains(char.ToUpper(c)))
+        result += c + " ";
+    else
+        result += "_ ";
+}
+
+    return result.TrimEnd();
+}
         
     public bool IsValid(string guessedLetters){
+		if(guessedLetters.Length != 10){
+			return false;
+		} else if (guessedLetters.Contains(" ")) {
+			return false;
+			
+		} else if (guessedLetters.Distinct().Count() != 10)
+		{
+   			 return false;
+		}
+		else 
+		{
         return true;
     }
 }
